d1d9fdd12bbc35a669b4c0e62699a3dd837aeeeb
Vikram Singh <vsingh18567@gmail.com>
2021-11-30 13:23:12 -0500

    removed unnecessary files, removed tox --cov-config and made a global test urls file instead of one for each folder

--- tests/urls.py
+++ tests/urls.py
@@ -2,5 +2,5 @@ from django.urls import include, path
 
 
 urlpatterns = [
-    path("", include("pennlabs.options.urls", namespace="django-labs-toolkit"))
+    path("/options", include("pennlabs.options.urls", namespace="options"))
 ]
