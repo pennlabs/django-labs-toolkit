85004307c11a9ecd04143a1c7f746f8bac174379
Vikram Singh <vsingh18567@gmail.com>
2022-03-20 15:27:30 -0400

    django-email-tools (#7)
    * reformatting imports
    
    * tests passing pls?
    
    * we love code cov

--- tests/urls.py
+++ tests/urls.py
@@ -4,4 +4,5 @@ from django.urls import include, path
 urlpatterns = [
     path("options/", include("pennlabs.options.urls", namespace="options")),
     path("shortener/", include("pennlabs.shortener.urls", namespace="shortener")),
+    path("email/", include("pennlabs.emailtools.urls", namespace="email_tools")),
 ]
